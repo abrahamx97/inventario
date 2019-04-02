8cd3412f7e208c29f16f0e5d7cf2c772a5c87949
Abraham <abrahamx97@gmail.com>
2019-04-02 16:12:43 -0600

    configuracion de variables

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,10 @@
 // Update with your config settings.
 const NODE_ENV = process.env.NODE_ENV
-const user = NODE_ENV === 'production' ? 'inventario' : 'root'
-const password = NODE_ENV === 'production' ? 'sandunga1000' : 'root'
-const database = NODE_ENV === 'production' ? 'inventario' : 'inventario'
-const port = NODE_ENV === 'production' ? 3306 : 3306
-const host = process.env.MYSQL_SERVICE_HOST
-
+const user = NODE_ENV === 'production' ? process.env.DB_USER : 'root'
+const password = NODE_ENV === 'production' ? process.env.DB_PASSWORD : 'root'
+const database = NODE_ENV === 'production' ? process.env.DB_NAME : 'inventario'
+const port = NODE_ENV === 'production' ? process.env.MYSQL_SERVICE_PORT : 3306
+const host = NODE_ENV === 'production' ? process.env.MYSQL_SERVICE_HOST : '127.0.0.1'
 
 console.log(JSON.stringify({
   user: user,
